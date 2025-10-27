703267c6c1fad663df7d161f485046e94b8d0c3f
jjh75607 <dkrak5738@naver.com>
2025-10-27 19:48:57 +0900

    init

---
+++ src/main/java/soon/fridgely/FridgelyBackEndApplication.java
@@ -0,0 +1,13 @@
+package soon.fridgely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgelyBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgelyBackEndApplication.class, args);
+    }
+
+}
