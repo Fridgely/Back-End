0026ad77b9f068bd66447ac74fab074856677c67
jjh75607 <dkrak5738@naver.com>
2025-11-03 17:20:21 +0900

    [#10] Feat: 로그인 로직 구현 - AuthService 구현 및 통합 테스트 작성 - MemberRepository.findByLoginId 추가 - LoginInfo DTO 추가

--- src/main/java/soon/fridgely/domain/member/repository/MemberRepository.java
+++ src/main/java/soon/fridgely/domain/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package soon.fridgely.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import soon.fridgely.domain.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
 }
