d26340abdf34ca2327f1e7a819883af2c3870aa7
jjh75607 <dkrak5738@naver.com>
2025-11-03 19:02:33 +0900

    [#12] Feat: 냉장고 생성 로직 구현 - RefrigeratorManager 구현 및 통합 테스트 작성

---
+++ src/test/java/soon/fridgely/domain/refrigerator/service/RefrigeratorManagerTest.java
@@ -0,0 +1,41 @@
+package soon.fridgely.domain.refrigerator.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import soon.fridgely.IntegrationTestSupport;
+import soon.fridgely.domain.member.entity.Member;
+import soon.fridgely.domain.refrigerator.entity.Refrigerator;
+import soon.fridgely.domain.refrigerator.repository.RefrigeratorRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RefrigeratorManagerTest extends IntegrationTestSupport {
+
+    @Autowired
+    private RefrigeratorManager refrigeratorManager;
+
+    @Autowired
+    private RefrigeratorRepository refrigeratorRepository;
+
+    @Test
+    void 냉장고를_생성한다() {
+        // given
+        Member member = createMember();
+
+        // when
+        Refrigerator refrigerator = refrigeratorManager.register(member);
+
+        // then
+        Refrigerator savedRefrigerator = refrigeratorRepository.findById(refrigerator.getId()).orElseThrow();
+        assertThat(savedRefrigerator)
+            .extracting("name")
+            .isEqualTo(member.getNickname() + "의 냉장고");
+    }
+
+    private Member createMember() {
+        return Member.builder()
+            .nickname("testNickname")
+            .build();
+    }
+
+}
