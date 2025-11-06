1a56510139605640f17175bad54c11a221545c98
jjh75607 <dkrak5738@naver.com>
2025-11-06 16:57:27 +0900

    [#15] Feat: 회원 가입 후 이벤트 발행 기능 구현 - MemberService.register 수정 - MemberServiceUnitTest 관련 테스트 수정

--- src/main/java/soon/fridgely/domain/member/service/MemberService.java
+++ src/main/java/soon/fridgely/domain/member/service/MemberService.java
@@ -1,11 +1,13 @@
 package soon.fridgely.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import soon.fridgely.domain.member.dto.MemberInfo;
 import soon.fridgely.domain.member.entity.Member;
 import soon.fridgely.domain.refrigerator.entity.Refrigerator;
+import soon.fridgely.domain.refrigerator.event.RefrigeratorCreatedEvent;
 import soon.fridgely.domain.refrigerator.service.MemberRefrigeratorLinker;
 import soon.fridgely.domain.refrigerator.service.RefrigeratorManager;
 
@@ -16,13 +18,22 @@ public class MemberService {
     private final MemberManager memberManager;
     private final RefrigeratorManager refrigeratorManager;
     private final MemberRefrigeratorLinker memberRefrigeratorLinker;
+    private final ApplicationEventPublisher eventPublisher;
 
-    // 회원 가입과 동시에 기본 냉장고를 생성하고, 회원과 냉장고를 연결
+    /**
+     * 회원 가입 후 기본 냉장고 생성 및 연결 완료 후 기본 카테고리 생성 이벤트 발행
+     *
+     * @param memberInfo 회원 정보
+     * @return 생성된 회원 ID
+     */
     @Transactional
     public Long register(MemberInfo memberInfo) {
         Member member = memberManager.register(memberInfo);
-        Refrigerator register = refrigeratorManager.register(member);
-        memberRefrigeratorLinker.linkToOwner(member, register);
+        Refrigerator refrigerator = refrigeratorManager.register(member);
+        memberRefrigeratorLinker.linkToOwner(member, refrigerator);
+
+        // 냉장고 생성 완료 후 Default category 생성
+        eventPublisher.publishEvent(new RefrigeratorCreatedEvent(refrigerator.getId(), member.getId()));
 
         return member.getId();
     }
