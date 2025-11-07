494b9c95e54be89747cbb2b5762b9069d8fbae22
jjh75607 <dkrak5738@naver.com>
2025-11-07 19:30:24 +0900

    [#16] Refactor: 로그인 회원 정보 인증 검증 로직 개선

--- src/main/java/soon/fridgely/global/support/annotation/LoginMemberResolver.java
+++ src/main/java/soon/fridgely/global/support/annotation/LoginMemberResolver.java
@@ -1,6 +1,7 @@
 package soon.fridgely.global.support.annotation;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,7 @@ public class LoginMemberResolver implements HandlerMethodArgumentResolver {
         WebDataBinderFactory binderFactory
     ) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
             throw new CoreException(ErrorType.AUTHENTICATION_FAILED);
         }
 
