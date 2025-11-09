aee5f6081f92122b1fbfe29f87e6a29ff00ffc7a
jjh75607 <dkrak5738@naver.com>
2025-11-09 20:30:39 +0900

    [#19] Feat: 카테고리 수정 API 추가 및 테스트 작성

--- src/test/java/soon/fridgely/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/soon/fridgely/domain/category/controller/CategoryControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import soon.fridgely.ControllerTestSupport;
 import soon.fridgely.domain.category.controller.dto.request.CategoryAddRequest;
+import soon.fridgely.domain.category.controller.dto.request.CategoryModifyRequest;
 import soon.fridgely.global.support.annotation.TestLoginMember;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -30,6 +32,25 @@ class CategoryControllerTest extends ControllerTestSupport {
             )
             .andDo(print())
             .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
+    }
+
+    @TestLoginMember
+    @Test
+    void 카테고리를_수정한다() throws Exception {
+        // given
+        var request = new CategoryModifyRequest("newCategoryName");
+        long refrigeratorId = 1L;
+        long categoryId = 1L;
+
+        // expected
+        mockMvc.perform(
+                patch(BASE_URL + "/" + refrigeratorId + "/categories/" + categoryId)
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
             .andExpect(jsonPath("$.result").value("SUCCESS"));
     }
 
