7b0268a29c8b085b8405d9d733ffa15826ef44c4
jjh75607 <dkrak5738@naver.com>
2025-11-10 19:32:35 +0900

    [#23] Feat: TruncateTablesListener에 AUTO_INCREMENT 초기화 기능 추가

--- src/test/java/soon/fridgely/TruncateTablesListener.java
+++ src/test/java/soon/fridgely/TruncateTablesListener.java
@@ -26,7 +26,19 @@ public class TruncateTablesListener extends AbstractTestExecutionListener {
     private void truncateTables(JdbcTemplate jdbcTemplate, List<String> truncateQueries) {
         execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY FALSE");
         truncateQueries.forEach(v -> execute(jdbcTemplate, v));
+        resetAutoIncrement(jdbcTemplate);
         execute(jdbcTemplate, "SET REFERENTIAL_INTEGRITY TRUE");
+    }
+
+    private void resetAutoIncrement(JdbcTemplate jdbcTemplate) {
+        List<String> tables = jdbcTemplate.queryForList(
+            "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'",
+            String.class
+        );
+
+        tables.forEach(tableName -> {
+            execute(jdbcTemplate, "ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1");
+        });
     }
 
     private void execute(JdbcTemplate jdbcTemplate, String query) {
