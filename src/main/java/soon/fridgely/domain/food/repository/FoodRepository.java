922249bf5987fd4f59fd0572638b3c5edf71722d
jjh75607 <dkrak5738@naver.com>
2025-11-17 17:31:52 +0900

    [#34] Feat: Food 커서 기반 페이징 조회 메서드 및 테스트 추가

--- src/main/java/soon/fridgely/domain/food/repository/FoodRepository.java
+++ src/main/java/soon/fridgely/domain/food/repository/FoodRepository.java
@@ -1,5 +1,7 @@
 package soon.fridgely.domain.food.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -24,5 +26,12 @@ public interface FoodRepository extends JpaRepository<Food, Long> {
     );
 
     Optional<Food> findByIdAndRefrigeratorIdAndStatus(long foodId, long refrigeratorId, EntityStatus status);
+
+    Slice<Food> findByRefrigeratorIdAndIdLessThanAndStatusOrderByIdDesc(
+        long refrigeratorId,
+        long id, // 마지막으로 조회된 Food의 ID
+        EntityStatus status,
+        Pageable pageable
+    );
 
 }
