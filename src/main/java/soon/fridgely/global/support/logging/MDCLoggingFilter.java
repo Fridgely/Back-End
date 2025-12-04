5b698b53e2b6ae20ae68d6924c12cb38b4b8fed3
jjh75607 <dkrak5738@naver.com>
2025-12-04 20:36:11 +0900

    [#66] Feat: MDCLoggingFilter를 OncePerRequestFilter로 변경하고 traceId 생성 로직 개선

--- src/main/java/soon/fridgely/global/support/logging/MDCLoggingFilter.java
+++ src/main/java/soon/fridgely/global/support/logging/MDCLoggingFilter.java
@@ -1,33 +1,37 @@
 package soon.fridgely.global.support.logging;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class MDCLoggingFilter implements Filter {
+public class MDCLoggingFilter extends OncePerRequestFilter {
 
     private static final String TRACE_ID = "traceId";
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
         try {
-            String traceId = generateTraceId();
-            MDC.put(TRACE_ID, traceId);
-            chain.doFilter(request, response);
+            MDC.put(TRACE_ID, generateTraceId());
+            filterChain.doFilter(request, response);
         } finally {
-            MDC.clear();
+            MDC.remove(TRACE_ID);
         }
     }
 
-    private static String generateTraceId() {
-        return UUID.randomUUID().toString();
+    private String generateTraceId() {
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 16);
     }
 
 }
