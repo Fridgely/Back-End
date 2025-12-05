f3189581c5736196f6315ef95715319e1ebdb25d
jjh75607 <dkrak5738@naver.com>
2025-12-05 19:57:56 +0900

    [#70] Feat: E2ETestSupport 인증 헤더 및 HttpEntity 생성 메서드 추가

--- src/test/java/soon/fridgely/E2ETestSupport.java
+++ src/test/java/soon/fridgely/E2ETestSupport.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
+import soon.fridgely.domain.auth.provider.TokenProvider;
+import soon.fridgely.domain.member.entity.Member;
 
 @ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
@@ -14,5 +18,32 @@ public abstract class E2ETestSupport {
 
     @Autowired
     protected TestRestTemplate testRestTemplate;
+
+    @Autowired
+    protected TokenProvider tokenProvider;
+
+    /**
+     * 인증 헤더가 포함된 HttpHeaders 생성
+     */
+    protected HttpHeaders createAuthHeaders(Member member) {
+        String token = tokenProvider.generateAllToken(member.getId(), member.getRole()).accessToken();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        return headers;
+    }
+
+    /**
+     * 인증 헤더가 포함된 HttpEntity 생성 (body 없음)
+     */
+    protected HttpEntity<Void> createAuthEntity(Member member) {
+        return new HttpEntity<>(createAuthHeaders(member));
+    }
+
+    /**
+     * 인증 헤더가 포함된 HttpEntity 생성 (body 포함)
+     */
+    protected <T> HttpEntity<T> createAuthEntity(T body, Member member) {
+        return new HttpEntity<>(body, createAuthHeaders(member));
+    }
 
 }
