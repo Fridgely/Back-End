2edf8f48b474f5dc23d5ad51ae9544914d0f1d2d
jjh75607 <dkrak5738@naver.com>
2025-12-07 19:12:10 +0900

    [#72] Feat: 매 정각마다 알림을 전송하는 스케줄링 기능 추가

--- src/main/java/soon/fridgely/domain/notification/service/NotificationService.java
+++ src/main/java/soon/fridgely/domain/notification/service/NotificationService.java
@@ -2,6 +2,7 @@ package soon.fridgely.domain.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import soon.fridgely.domain.notification.batch.BatchResult;
 import soon.fridgely.domain.notification.batch.NotificationBatchExecutor;
@@ -17,6 +18,7 @@ public class NotificationService {
     private final NotificationBatchExecutor notificationBatchExecutor;
     private final NotificationProcessor notificationProcessor;
 
+    @Scheduled(cron = "0 0 * * * *") // 매 정각마다 실행
     public void sendScheduledAlerts() {
         LocalTime now = LocalTime.now();
         LocalTime start = TimeRangeUtils.startOfHour(now);
