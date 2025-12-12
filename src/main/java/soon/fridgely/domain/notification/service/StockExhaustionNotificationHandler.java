fbf23d897215a242b88c9698d3d73e115f39764f
jjh75607 <dkrak5738@naver.com>
2025-12-12 17:04:15 +0900

    [#83] Feat: 재고 소진 시 알림 프로세서 연동

--- src/main/java/soon/fridgely/domain/notification/service/StockExhaustionNotificationHandler.java
+++ src/main/java/soon/fridgely/domain/notification/service/StockExhaustionNotificationHandler.java
@@ -1,19 +1,19 @@
 package soon.fridgely.domain.notification.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import soon.fridgely.domain.food.entity.Food;
 import soon.fridgely.domain.food.service.FoodStockHandler;
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class StockExhaustionNotificationHandler implements FoodStockHandler {
 
+    private final NotificationProcessor notificationProcessor;
+
     @Override
     public void onStockExhausted(Food food) {
-        throw new UnsupportedOperationException("Unsupported onStockExhausted");
+        notificationProcessor.processStockExhaustion(food);
     }
 
 }
