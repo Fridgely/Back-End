c028e5a082864bf6ff08d720b80d55d071bc234e
jjh75607 <dkrak5738@naver.com>
2025-12-31 18:12:02 +0900

    [#93] Refactor: 냉장고 도메인 통합/단위 테스트에 Fixture Monkey 적용

--- src/test/java/soon/fridgely/domain/refrigerator/service/MemberRefrigeratorFinderIntegrationTest.java
+++ src/test/java/soon/fridgely/domain/refrigerator/service/MemberRefrigeratorFinderIntegrationTest.java
@@ -1,9 +1,9 @@
 package soon.fridgely.domain.refrigerator.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import soon.fridgely.domain.member.entity.Member;
-import soon.fridgely.domain.member.entity.MemberRole;
 import soon.fridgely.domain.member.repository.MemberRepository;
 import soon.fridgely.domain.refrigerator.entity.MemberRefrigerator;
 import soon.fridgely.domain.refrigerator.entity.Refrigerator;
@@ -17,6 +17,9 @@ import soon.fridgely.global.support.exception.ErrorType;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static soon.fridgely.global.support.fixture.MemberFixture.member;
+import static soon.fridgely.global.support.fixture.MemberRefrigeratorFixture.memberRefrigerator;
+import static soon.fridgely.global.support.fixture.RefrigeratorFixture.refrigerator;
 
 class MemberRefrigeratorFinderIntegrationTest extends IntegrationTestSupport {
 
@@ -32,63 +35,76 @@ class MemberRefrigeratorFinderIntegrationTest extends IntegrationTestSupport {
     @Autowired
     private MemberRefrigeratorRepository memberRefrigeratorRepository;
 
+    private Member member;
+    private Refrigerator refrigerator;
+
+    @BeforeEach
+    void setUp() {
+        this.member = memberRepository.save(
+            member(fixtureMonkey).sample()
+        );
+        this.refrigerator = refrigeratorRepository.save(
+            refrigerator(fixtureMonkey).sample()
+        );
+    }
+
     @Test
     void 내가_속한_모든_냉장고_목록을_조회한다() {
         // given
-        Member me = createMember("me");
-        Member other = createMember("other");
-        memberRepository.saveAll(List.of(me, other));
+        Member otherMember = memberRepository.save(
+            member(fixtureMonkey).sample()
+        );
 
-        Refrigerator myFridge1 = Refrigerator.register(me.getNickname());
-        Refrigerator myFridge2 = Refrigerator.register(me.getNickname());
-        Refrigerator otherFridge = Refrigerator.register(other.getNickname());
-        refrigeratorRepository.saveAll(List.of(myFridge1, myFridge2, otherFridge));
+        Refrigerator myRefrigerator2 = refrigeratorRepository.save(
+            refrigerator(fixtureMonkey).sample()
+        );
+        Refrigerator otherFridge = refrigeratorRepository.save(
+            refrigerator(fixtureMonkey).sample()
+        );
 
-        memberRefrigeratorRepository.save(MemberRefrigerator.link(me, myFridge1, RefrigeratorRole.OWNER));
-        memberRefrigeratorRepository.save(MemberRefrigerator.link(me, myFridge2, RefrigeratorRole.MEMBER));
-        memberRefrigeratorRepository.save(MemberRefrigerator.link(other, otherFridge, RefrigeratorRole.OWNER));
+        memberRefrigeratorRepository.saveAll(List.of(
+            memberRefrigerator(fixtureMonkey, refrigerator, member)
+                .set("role", RefrigeratorRole.OWNER)
+                .sample(),
+            memberRefrigerator(fixtureMonkey, myRefrigerator2, member)
+                .set("role", RefrigeratorRole.MEMBER)
+                .sample(),
+            memberRefrigerator(fixtureMonkey, otherFridge, otherMember) // 다른 사람의 냉장고
+                .sample()
+        ));
 
         // when
-        List<MemberRefrigerator> result = memberRefrigeratorFinder.findAllByMemberId(me.getId());
+        List<MemberRefrigerator> result = memberRefrigeratorFinder.findAllByMemberId(member.getId());
 
         // then
         assertThat(result).hasSize(2)
-            .extracting("refrigerator.name", "role")
-            .containsExactly(
-                tuple("me의 냉장고", RefrigeratorRole.OWNER),
-                tuple("me의 냉장고", RefrigeratorRole.MEMBER)
+            .extracting("refrigerator.id", "role")
+            .containsExactlyInAnyOrder(
+                tuple(refrigerator.getId(), RefrigeratorRole.OWNER),
+                tuple(myRefrigerator2.getId(), RefrigeratorRole.MEMBER)
             );
     }
 
     @Test
     void 특정_냉장고에_대한_멤버십_정보를_조회한다() {
         // given
-        Member member = createMember("test");
-        memberRepository.save(member);
-
-        Refrigerator refrigerator = Refrigerator.register(member.getNickname());
-        refrigeratorRepository.save(refrigerator);
-
-        memberRefrigeratorRepository.save(MemberRefrigerator.link(member, refrigerator, RefrigeratorRole.OWNER));
+        memberRefrigeratorRepository.save(
+            memberRefrigerator(fixtureMonkey, refrigerator, member)
+                .set("role", RefrigeratorRole.OWNER)
+                .sample()
+        );
 
         // when
         MemberRefrigerator result = memberRefrigeratorFinder.findByMemberIdAndRefrigeratorId(member.getId(), refrigerator.getId());
 
         // then
         assertThat(result).isNotNull()
-            .extracting("refrigerator.name", "role")
-            .containsExactly(refrigerator.getName(), RefrigeratorRole.OWNER);
+            .extracting("refrigerator.id", "role")
+            .containsExactly(refrigerator.getId(), RefrigeratorRole.OWNER);
     }
 
     @Test
     void 멤버십_정보가_존재하지_않으면_예외가_발생한다() {
-        // given
-        Member member = createMember("test");
-        memberRepository.save(member);
-
-        Refrigerator refrigerator = Refrigerator.register("Test Fridge");
-        refrigeratorRepository.save(refrigerator);
-
         // expected
         assertThatThrownBy(() -> memberRefrigeratorFinder.findByMemberIdAndRefrigeratorId(member.getId(), refrigerator.getId()))
             .isInstanceOf(CoreException.class)
@@ -99,13 +115,11 @@ class MemberRefrigeratorFinderIntegrationTest extends IntegrationTestSupport {
     @Test
     void 냉장고와_멤버의_연결을_확인한다() {
         // given
-        Member member = createMember("test");
-        memberRepository.save(member);
-
-        Refrigerator refrigerator = Refrigerator.register(member.getNickname());
-        refrigeratorRepository.save(refrigerator);
-
-        memberRefrigeratorRepository.save(MemberRefrigerator.link(member, refrigerator, RefrigeratorRole.MEMBER));
+        memberRefrigeratorRepository.save(
+            memberRefrigerator(fixtureMonkey, refrigerator, member)
+                .set("role", RefrigeratorRole.MEMBER)
+                .sample()
+        );
 
         // when
         boolean exists = memberRefrigeratorFinder.existsByRefrigeratorIdAndMemberId(refrigerator.getId(), member.getId());
@@ -116,28 +130,11 @@ class MemberRefrigeratorFinderIntegrationTest extends IntegrationTestSupport {
 
     @Test
     void 냉장고와_멤버가_연결되지_않은_경우_false를_반환한다() {
-        // given
-
-        Member member = createMember("test");
-        memberRepository.save(member);
-
-        Refrigerator refrigerator = Refrigerator.register(member.getNickname());
-        refrigeratorRepository.save(refrigerator);
-
         // when
         boolean exists = memberRefrigeratorFinder.existsByRefrigeratorIdAndMemberId(refrigerator.getId(), member.getId());
 
         // then
         assertThat(exists).isFalse();
-    }
-
-    private Member createMember(String nickname) {
-        return Member.builder()
-            .loginId(nickname)
-            .password("pw")
-            .nickname(nickname)
-            .role(MemberRole.MEMBER)
-            .build();
     }
 
 }
