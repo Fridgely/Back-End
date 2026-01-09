9fef03eb9a4c8c1c26abb572ef4c7145815dcd59
jjh75607 <dkrak5738@naver.com>
2026-01-09 17:43:05 +0900

    [#101] Docs: category 패키지 관련 명세 작성

--- src/main/java/soon/fridgely/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/soon/fridgely/domain/category/dto/response/CategoryResponse.java
@@ -1,13 +1,22 @@
 package soon.fridgely.domain.category.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import soon.fridgely.domain.category.entity.Category;
 
 import java.util.List;
 
+@Schema(description = "카테고리 목록 응답")
 public record CategoryResponse(
+
+    @Schema(description = "카테고리 ID", example = "1")
     long id,
+
+    @Schema(description = "카테고리 이름", example = "유제품")
     String name,
+
+    @Schema(description = "기본 카테고리 여부", example = "true")
     boolean isDefaultType
+
 ) {
 
     public static List<CategoryResponse> from(List<Category> categories) {
