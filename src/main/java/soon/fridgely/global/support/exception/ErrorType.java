eeff18fe10179b8e7d36869cb652e43e74ecae10
JungSoonJae <34623431+jjh75607@users.noreply.github.com>
2026-03-15 20:25:31 +0900

    Fix: 리프레시 토큰 평문 저장 취약점 수정  (#168)
    * [#167] Fix: 리프레시 토큰 평문 저장 보안 취약점 수정
    
    * [#167] Fix: refreshToken 검증 시 null 체크 추가
    
    * [#167] Refactor: 중복된 테스트 케이스 제거

--- src/main/java/soon/fridgely/global/support/exception/ErrorType.java
+++ src/main/java/soon/fridgely/global/support/exception/ErrorType.java
@@ -11,6 +11,7 @@ public enum ErrorType {
 
     // 공통 오류
     DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.", LogLevel.ERROR),
+    INTERNAL_CRYPTO_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 암호화 처리 중 오류가 발생했습니다.", LogLevel.ERROR),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청이 올바르지 않습니다.", LogLevel.INFO),
     NOT_FOUND_DATA(HttpStatus.BAD_REQUEST, "해당 데이터를 찾을 수 없습니다.", LogLevel.ERROR),
     INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "요청 값의 타입이 올바르지 않습니다.", LogLevel.INFO),
