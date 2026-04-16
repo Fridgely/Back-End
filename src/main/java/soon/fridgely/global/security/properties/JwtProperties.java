8a2ec8360dcda905386721023d3e706edf1f4953
jjh75607 <dkrak5738@naver.com>
2026-04-16 15:47:59 +0900

    [#181] Feat: JwtProperties에 유효성 검증 추가

--- src/main/java/soon/fridgely/global/security/properties/JwtProperties.java
+++ src/main/java/soon/fridgely/global/security/properties/JwtProperties.java
@@ -1,11 +1,15 @@
 package soon.fridgely.global.security.properties;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
+@Validated
 @ConfigurationProperties(prefix = "spring.jwt")
 public record JwtProperties(
+    @NotBlank(message = "JWT secretKey는 필수입니다.")
     String secretKey,
     Duration accessTokenExpirationTime,
     Duration refreshTokenExpirationTime
