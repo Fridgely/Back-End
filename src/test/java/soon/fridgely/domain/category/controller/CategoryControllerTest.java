1d1a69b6d531c478e03bc0ce7202b03dd3c2e0a2
jjh75607 <dkrak5738@naver.com>
2026-04-16 15:49:08 +0900

    [#181] Feat: 카테고리 추가, 수정, 삭제 기능에 대한 테스트 추가

--- src/test/java/soon/fridgely/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/soon/fridgely/domain/category/controller/CategoryControllerTest.java
@@ -1,6 +1,9 @@
 package soon.fridgely.domain.category.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import soon.fridgely.domain.category.dto.request.CategoryAddRequest;
+import soon.fridgely.domain.category.dto.request.CategoryModifyRequest;
 import soon.fridgely.domain.category.dto.response.CategoryDetailResponse;
 import soon.fridgely.domain.category.dto.response.CategoryResponse;
 import soon.fridgely.domain.refrigerator.dto.command.MemberRefrigeratorKey;
@@ -12,7 +15,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +23,96 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class CategoryControllerTest extends ControllerTestSupport {
 
     private static final String BASE_URL = "/api/v1/refrigerators";
+
+    @TestLoginMember
+    @Test
+    void 카테고리를_추가한다() throws Exception {
+        // given
+        long refrigeratorId = 1L;
+        var request = new CategoryAddRequest("간식류");
+
+        // expected
+        mockMvc.perform(
+                post(BASE_URL + "/" + refrigeratorId + "/categories")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
+    }
+
+    @TestLoginMember
+    @Test
+    void 카테고리_이름이_빈_문자열이면_추가에_실패한다() throws Exception {
+        // given
+        long refrigeratorId = 1L;
+        var invalidRequest = new CategoryAddRequest("");
+
+        // expected
+        mockMvc.perform(
+                post(BASE_URL + "/" + refrigeratorId + "/categories")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(invalidRequest))
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.result").value("ERROR"));
+    }
+
+    @TestLoginMember
+    @Test
+    void 카테고리를_수정한다() throws Exception {
+        // given
+        long refrigeratorId = 1L;
+        long categoryId = 1L;
+        var request = new CategoryModifyRequest("음료류");
+
+        // expected
+        mockMvc.perform(
+                patch(BASE_URL + "/" + refrigeratorId + "/categories/" + categoryId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
+    }
+
+    @TestLoginMember
+    @Test
+    void 카테고리_새_이름이_빈_문자열이면_수정에_실패한다() throws Exception {
+        // given
+        long refrigeratorId = 1L;
+        long categoryId = 1L;
+        var invalidRequest = new CategoryModifyRequest("");
+
+        // expected
+        mockMvc.perform(
+                patch(BASE_URL + "/" + refrigeratorId + "/categories/" + categoryId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(invalidRequest))
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.result").value("ERROR"));
+    }
+
+    @TestLoginMember
+    @Test
+    void 카테고리를_삭제한다() throws Exception {
+        // given
+        long refrigeratorId = 1L;
+        long categoryId = 1L;
+
+        // expected
+        mockMvc.perform(
+                delete(BASE_URL + "/" + refrigeratorId + "/categories/" + categoryId)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"));
+    }
 
     @TestLoginMember
     @Test
