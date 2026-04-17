68fe70889b49af4a3b69956a5357d8792728ef76
jjh75607 <dkrak5738@naver.com>
2026-04-17 18:38:32 +0900

    [#181] Feat: JwtProperties에 accessTokenExpirationTime 및 refreshTokenExpirationTime 유효성 검증 추가

--- src/main/java/soon/fridgely/global/security/properties/JwtProperties.java
+++ src/main/java/soon/fridgely/global/security/properties/JwtProperties.java
@@ -1,6 +1,7 @@
 package soon.fridgely.global.security.properties;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -11,7 +12,9 @@ import java.time.Duration;
 public record JwtProperties(
     @NotBlank(message = "JWT secretKey는 필수입니다.")
     String secretKey,
+    @NotNull(message = "JWT accessTokenExpirationTime은 필수입니다.")
     Duration accessTokenExpirationTime,
+    @NotNull(message = "JWT refreshTokenExpirationTime은 필수입니다.")
     Duration refreshTokenExpirationTime
 ) {
 
