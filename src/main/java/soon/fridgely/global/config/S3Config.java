ebc47d2cbfd479609ab3dbe3b479a402c6785764
jjh75607 <dkrak5738@naver.com>
2026-04-29 23:55:03 +0900

    [#186] Fix: S3 자격증명 미주입으로 인한 파일 업로드 실패 수정

--- src/main/java/soon/fridgely/global/config/S3Config.java
+++ src/main/java/soon/fridgely/global/config/S3Config.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -24,8 +26,12 @@ public class S3Config {
     @Bean(destroyMethod = "close")
     public S3Client s3Client(S3Properties s3Properties) {
         Region region = Region.of(s3Properties.region().staticRegion());
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(s3Properties.credentials().accessKey(), s3Properties.credentials().secretKey())
+        );
         return S3Client.builder()
             .region(region)
+            .credentialsProvider(credentialsProvider)
             .build();
     }
 
@@ -33,8 +39,12 @@ public class S3Config {
     @Bean(destroyMethod = "close")
     public S3Presigner s3Presigner(S3Properties s3Properties) {
         Region region = Region.of(s3Properties.region().staticRegion());
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(s3Properties.credentials().accessKey(), s3Properties.credentials().secretKey())
+        );
         return S3Presigner.builder()
             .region(region)
+            .credentialsProvider(credentialsProvider)
             .build();
     }
 
